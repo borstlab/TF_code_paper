7cf9c4fd5e94fcc6e4b1b12225dc66da504b90fa
nhoermann <58261267+nhoermann@users.noreply.github.com>
2020-04-16 15:14:33 +0200

    Update Calculating_dendritic_orientation.py

--- Calculating_dendritic_orientation.py
+++ Calculating_dendritic_orientation.py
@@ -1,9 +1,7 @@
-from skimage import data, exposure, filters, io, segmentation, external
+from skimage import io
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import seaborn as sns
-%matplotlib inline
 
 def Vector(x, y, image, int_thresh):
     
@@ -31,6 +29,8 @@ def Vector(x, y, image, int_thresh):
     ## Substract the dbp index from every pixel to calculate the vectors
     
     norm_pixel_values = pixel_co - dbp
+
+    ## Change the orientation of the y-axis
 
     norm_pixel_values[:,0]*= -1
    
@@ -140,24 +140,3 @@ def dendritic_polarplot(sine, cos, nbins, norm_ori_vec, DSI, Name):
     plt.show()
     
     return True
-
-
-## How to use the script
-# Download the Example_Neuron_tif
-# Vector:
-#   x:          x coordinate of dendritic branching point
-#   y:          y coordinate of dendritic branching point
-#   image:      path to image file used in the analysis
-#   int_thresh: intensity threshold used to filter the image (usually between 0-255)
-# dendritic_polarplot:
-#   sine:           Sine coordinates used to bin the pixel values
-#   cos:            Cosine coordinates used to bin the pixel values
-#   nbins:          Number of bins used for the polar plot
-#   norm_ori_vec:   Normalized orientation vector
-#   DSI:            Calculated DSI of dendrite shape
-#   Name:           Name of the output .png file
-# 
-# 
-# norm_ori_vec, norm_pixel_values, DGI = Vector(96, 156, 'path/to/Example_Neuron.tif', 50)
-# 
-# dendritic_polarplot(norm_pixel_values[:,0], norm_pixel_values[:,1], 16, norm_ori_vec, DGI, 'Example_Neuron')
